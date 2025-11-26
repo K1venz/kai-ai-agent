c2ca83760bb47c00d847c802c3aa5251b727b8ac
k1venz <k1venz@outlook.com>
2025-11-26 20:53:32 +0800

    First

---
+++ src/main/java/com/zk/agent/KaiiAiAgentApplication.java
@@ -0,0 +1,13 @@
+package com.zk.agent;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KaiiAiAgentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KaiiAiAgentApplication.class, args);
+    }
+
+}
